2192a2d24c95c1441ce8731fba9b78b934fb2cf5
Zoe Goldschmidt <zoe.gld@gmail.com>
2019-11-25 18:19:44 +0000

    new links in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'flats#index'
+  root to: 'pages#home'
 
   resources :users, only: [:new, :edit, :update]
   get 'my_account', to: 'pages#my_account'
