faf2ff2b894133f656569128fdcd77916513a8e6
Zoe Goldschmidt <zoe.gld@gmail.com>
2019-11-26 17:52:25 +0000

    rentals policy

--- app/policies/rental_policy.rb
+++ app/policies/rental_policy.rb
@@ -1,4 +1,20 @@
 class RentalPolicy < ApplicationPolicy
+  def new?
+    record.flat.user == user
+  end
+
+  def create?
+    new?
+  end
+
+  def join_flat?
+    record.tenant_email == user.email
+  end
+
+  def accept_rental?
+    join_flat?
+  end
+
   class Scope < Scope
     def resolve
       scope.all
