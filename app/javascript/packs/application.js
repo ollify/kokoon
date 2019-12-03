1496bcb5b7c2b56b9c52b4508c41d8c30e97ce10
Isolda Saez <isoldilla@gmail.com>
2019-12-03 12:37:52 +0000

    fixed js files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,7 @@
 import "bootstrap";
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initFlatpickr } from '../plugins/init_flatpickr';
-import { toggleAsideMethod } from '../plugins/aside';
 
 initAutocomplete();
-toggleAsideMethod();
+
 
