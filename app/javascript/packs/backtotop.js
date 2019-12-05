2973582b8f0bdb35cfb97857e2a5c4f6d0ca9749
Zoe Goldschmidt <zoe.gld@gmail.com>
2019-12-05 18:46:25 +0000

    toggleaside fix

--- app/javascript/packs/backtotop.js
+++ app/javascript/packs/backtotop.js
@@ -1,32 +1,32 @@
-console.log("Back to top");
+// console.log("Back to top");
 
-const initBackToTop = () => {
-    const toggleAside = document.querySelector("#toggle-aside");
-    const btnTop = document.querySelector("#btn-backtop");
-    const userAside = document.querySelector(".account-aside");
-    const userMain = document.querySelector(".account-main");
-    const myProperties = document.querySelector(".myproperties");
-    const myFlats = document.querySelector(".myflats");
-    const leftArrow = document.getElementById("image-arrow");
-    const rightArrow = document.getElementById("image-arrow-right");
-    const gridRow = document.querySelector(".row");
+// const initBackToTop = () => {
+//     const toggleAside = document.querySelector("#toggle-aside");
+//     const btnTop = document.querySelector("#btn-backtop");
+//     const userAside = document.querySelector(".account-aside");
+//     const userMain = document.querySelector(".account-main");
+//     const myProperties = document.querySelector(".myproperties");
+//     const myFlats = document.querySelector(".myflats");
+//     const leftArrow = document.getElementById("image-arrow");
+//     const rightArrow = document.getElementById("image-arrow-right");
+//     const gridRow = document.querySelector(".row");
 
-    toggleAside.addEventListener("click", (event) => {
-        userAside.classList.toggle("hide");
-        userMain.classList.toggle("hide");
-        myProperties.classList.toggle("expanded");
-        myFlats.classList.toggle("expanded");
-        gridRow.classList.toggle("expanded");
+//     toggleAside.addEventListener("click", (event) => {
+//         userAside.classList.toggle("hide");
+//         userMain.classList.toggle("hide");
+//         myProperties.classList.toggle("expanded");
+//         myFlats.classList.toggle("expanded");
+//         gridRow.classList.toggle("expanded");
 
-        if (userAside.classList.contains("hide")) {
-          toggleAside.innerHTML = `<img src=${rightArrow.src}>`
-        }
+//         if (userAside.classList.contains("hide")) {
+//           toggleAside.innerHTML = `<img src=${rightArrow.src}>`
+//         }
 
-        else {
-          toggleAside.innerHTML = `<img src=${leftArrow.src}>`
-        }
-    });
+//         else {
+//           toggleAside.innerHTML = `<img src=${leftArrow.src}>`
+//         }
+//     });
 
-  };
+//   };
 
-initBackToTop();
+//initBackToTop();
